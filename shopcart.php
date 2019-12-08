b0ba3046ded5d2f885596ce08e3c7c5ac4b726b6
Yerzhan Orazayev <yerzhan1993@gmail.com>
2019-12-07 23:18:06 -0800

    adding shopcart php

---
+++ shopcart.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Darknet shop</title>
+        <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
+    <body>
+        <div id="menu">
+            <div id="darknet_logo">
+                <a href="index.php">
+                    <img id="darknet_logo" src="https://tinyurl.com/rgqfw2w" />
+                </a>
+            </div>
+            <div class="menu_button">
+                <input id="user_account" type="image" src="https://tinyurl.com/r2fwt85" />
+            </div>
+            <div class="menu_button">
+                <input id="shop_cart" type="image" src="https://tinyurl.com/sut2whc" />
+            </div>
+        </div>
+        <div id="central">
+            <?php
+            ?>
+        </div>
+    </body>
+</html>
